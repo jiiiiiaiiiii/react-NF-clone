34c556732b34f642561c7e54bd506f9ebd3e3b43
aqubi <msp827@naver.com>
2023-12-06 17:22:59 +0900

    const 컴포넌트명 = styled.태그명`CSS 코드`;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div>
+  const Father = styled.div`
+    display: flex;
+  `;
 
-    </div>
+  const BoxOne = styled.div`
+    background-color: teal;
+    width: 100px;
+    height: 100px;
+  `;
+
+  const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;  
+`;
+
+  return (
+    <Father>
+      <BoxOne>
+      <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
   );
 }
 
