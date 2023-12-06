bc2d74e03073bf36a1a4a4c549a368c655a575df
aqubi <msp827@naver.com>
2023-12-06 18:49:28 +0900

    Themes: 다크모드/라이트모드
    # index.js
    
    import { ThemeProvider } from 'styled-components';
    
    /* 테마 정의 */
    /* 테마별 property명 통일 */
    
    const darkTheme = {
      textColor: 'whitesmoke',
      backgroundColor: '#111',
    }
    
    const lightTheme = {
      textColor: '#111',
      backgroundColor: 'whitesmoke',
    }
    
    /* ThemeProvider로 component(App.js) 감싸주고, props로 theme 전달 */
    
    <ThemeProvider theme={lightTheme}>
        <App />
    </ThemeProvider>
    
    # App.js
    
      const Title = styled.h1`
        color: ${(props) => props.theme.textColor};
      `;
    
    => 상위 component (ThemeProvider)의 props로 접근

--- src/App.js
+++ src/App.js
@@ -17,33 +17,22 @@ function App() {
     }
   `;
 
+  const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
+
   const Wrapper = styled.div`
     display: flex;
+    height: 100vh;
+    width: 100vw;
     justify-content: center;
     align-items: center;
-  `;
-
-  const Emoji = styled.span`
-  font-size: 36px;
-  `;
-
-  const Box = styled.div`
-    height: 200px;
-    width: 200px;
-    background-color: tomato;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    animation: ${rotateAnimation} 1s linear infinite;
-    ${Emoji}:hover {font-size: 98px;}
+    background-color: ${(props) => props.theme.backgroundColor};
   `;
 
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
