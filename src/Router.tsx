a943e3aeb4eba3e567306dd4b2f6c06506cccb47
aqubi <msp827@naver.com>
2023-12-12 21:39:53 +0900

    createBrowserRouter : Router를 Array 형식으로 표현(-> JS Object 형식)
    # Router.tsx
    
    const router = createBrowserRouter([
    	{
    		// 첫페이지 = 전체 Router들의 container
    		path: '/',
    		element: <Root />,
    		children: [
    			{
    				path: '',
    				element: <Home />
    			},
    			{
    				path: 'about',
    				element: <About />
    			},
    		]
    	}
    ]);

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, createBrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './screens/Home';
 import About from './screens/About';
+import Root from './Root';
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Header />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/about' element={<About />} />
-			</Routes>
-    </BrowserRouter>
-  );
-}
+// Router를 Array 형식으로 표현(-> JS Object 형식)
+const router = createBrowserRouter([
+	{
+		// 첫페이지 = 전체 Router들의 container
+		path: '/',
+		element: <Root />,
+		children: [
+			{
+				path: '',
+				element: <Home />
+			},
+			{
+				path: 'about',
+				element: <About />
+			},
+		]
+	}
+]);
 
-export default Router;
+export default router;
