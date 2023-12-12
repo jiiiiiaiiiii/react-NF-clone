1b695d95fcc6c6454706c67245ed42e37bdc2f50
aqubi <msp827@naver.com>
2023-12-12 20:13:24 +0900

    Forms
    const onChange = (e: React.FormEvent<HTMLInputElement>) => {
        // const value = e.currentTarget.value; // 아래와 같은 코드
        const { currentTarget: {value} } = e; };
    
    1. ES6 문법
    const value = event.currentTarget.value
    const {value} = event.currentTarget
    const {currentTarget:{value}} = event
    
    1-1.
    const value = event.currentTarget.value;
    const tagName = event.currentTarget.tagName;
    const width = event.currentTarget.width;
    const id = event.currentTarget.id;
    
    /*---아래와 같이 바꿔쓸 수 있음---*/
    
    const {
    currentTarget: {value, tagName, width, id}
    } = event;

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import Circle from './Circle';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    // const value = e.currentTarget.value; // 아래와 같은 코드
+    const { currentTarget: {value} } = e;
+    setValue(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('hello', value);
+    
+  };
 
   return (
     <div>
-      <Circle borderColor='yellow' bgColor='teal'/>
-      <Circle text='i`m here' bgColor='tomato'/>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type='text'
+          placeholder='username'
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
