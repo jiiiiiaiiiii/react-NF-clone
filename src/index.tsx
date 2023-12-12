704e502dbe599b486b01565c287996309005dcd3
aqubi <msp827@naver.com>
2023-12-12 21:05:40 +0900

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from 'styled-components';
-import { darkThmem, lightThmem } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <ThemeProvider theme={darkThmem}>
-        <App />
-      </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
