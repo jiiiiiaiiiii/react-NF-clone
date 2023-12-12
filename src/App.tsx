5943b01114023016be97a71062fefda3741918c5
aqubi <msp827@naver.com>
2023-12-12 20:30:41 +0900

    Themes: TypeScript with Styled-Component
    1. styled.d.ts 를 만든다. (참고: d.ts 는 declaration file)
    2. theme.ts (테마) 생성: theme 속성 정의
    3. index.tsx 에 2에서 만든 테마 적용: ThemeProvider 사용
    4. App.tsx 에서 props로 받아 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 
 function App() {
-  const [value, setValue] = useState('');
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    // const value = e.currentTarget.value; // 아래와 같은 코드
-    const { currentTarget: {value} } = e;
-    setValue(value);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log('hello', value);
-    
-  };
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type='text'
-          placeholder='username'
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components/dist/types'
+
+export const lightThmem: DefaultTheme = {
+	textColor: 'black',
+	bgColor: 'white',
+	btnColor: 'tomato,'
+};
+
+export const darkThmem: DefaultTheme = {
+	textColor: 'white',
+	bgColor: 'black',
+	btnColor: 'teal',
+};
