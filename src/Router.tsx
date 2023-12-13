8a8a6a437610b488be9e8982455d02d14177e3ef
aqubi <msp827@naver.com>
2023-12-13 18:44:00 +0900

    useParams
    - useParams 훅은 < Route path >와 일치하는 현재 URL에서 동적 매개변수의 key/value 쌍 객체를 반환
    (※ 하위 경로는 상위 경로에서 모든 매개변수를 상속)

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import About from './screens/About';
 import Root from './Root';
 import NotFound from './screens/NotFound';
 import ErrorComponent from './components/ErrorComponent';
+import User from './screens/users/User';
 
 // Router를 Array 형식으로 표현(-> JS Object 형식)
 const router = createBrowserRouter([
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
 				path: 'about',
 				element: <About />
 			},
+			{
+				path:'users/:userId',
+				element: <User />
+			}
 		],
 		errorElement: <NotFound />
 	}
