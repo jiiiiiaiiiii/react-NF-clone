b10321a54b360306916cb1b640395ff7581e3123
aqubi <msp827@naver.com>
2023-12-22 18:33:55 +0900

    useSearchParams
    useSearchParams: search 파라미터를 수정할 수 있게 도움 -> 필터, 페이지네이션
    - URLSearchParams: utility method 제공

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { users } from '../db';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 function Home() {
+
+// useSearchParams: search 파라미터를 수정할 수 있게 도움 -> filter, pagination
+// -> URLSearchParams: utility method 제공
+  const [readSearchParams, setSearchParams] = useSearchParams();
+
+  setTimeout(() => {
+    setSearchParams({
+      day: 'today',
+      tomorrow: '123'
+    })
+  }, 3000) // 3초 후 url 변경 -> http://localhost:3000/?day=today&tomorrow=123
+  
   return (
     <div>
       <h1>Users</h1>
