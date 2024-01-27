769bb499cf2c2d11d2067e78a6fc1b9d296b8702
aqubi <msp827@naver.com>
2024-01-27 12:41:44 +0900

    Router Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Routes/Home';
+import TV from './Routes/TV';
+import Search from './Routes/Search';
 
+function App() {
   return (
-    null
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/tv' element={<TV />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
